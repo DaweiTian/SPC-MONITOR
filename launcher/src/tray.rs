cc96d4e5e5fc603e074ed4da5f6f728da63fd7f2
Erribaba Tian <tianwenxiao@wo.cn>
2026-08-21 20:14:47 +0800

    feat: rewrite tray with Tauri v2 menu API

--- launcher/src/tray.rs
+++ launcher/src/tray.rs
@@ -2,68 +2,87 @@ use crate::service::ServiceManager;
 use log::error;
 use std::sync::Arc;
 use tauri::{
-    CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+    menu::{Menu, MenuItem, PredefinedMenuItem},
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    Emitter, Manager,
 };
 
-pub fn create_system_tray() -> SystemTray {
-    let show = CustomMenuItem::new("show".to_string(), "显示窗口");
-    let start = CustomMenuItem::new("start".to_string(), "启动服务");
-    let stop = CustomMenuItem::new("stop".to_string(), "停止服务");
-    let open_browser = CustomMenuItem::new("open_browser".to_string(), "打开浏览器");
-    let quit = CustomMenuItem::new("quit".to_string(), "退出");
+pub fn create_tray(app: &tauri::App, service_manager: Arc<ServiceManager>) -> tauri::Result<()> {
+    let show = MenuItem::with_id(app, "show", "显示窗口", true, None::<&str>)?;
+    let start = MenuItem::with_id(app, "start", "启动服务", true, None::<&str>)?;
+    let stop = MenuItem::with_id(app, "stop", "停止服务", true, None::<&str>)?;
+    let open_browser = MenuItem::with_id(app, "open_browser", "打开浏览器", true, None::<&str>)?;
+    let toggle_widget = MenuItem::with_id(app, "toggle_widget", "桌面小组件", true, None::<&str>)?;
+    let quit = MenuItem::with_id(app, "quit", "退出", true, None::<&str>)?;
 
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(show)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(start)
-        .add_item(stop)
-        .add_item(open_browser)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(quit);
+    let menu = Menu::with_items(
+        app,
+        &[
+            &show,
+            &PredefinedMenuItem::separator(app)?,
+            &start,
+            &stop,
+            &open_browser,
+            &toggle_widget,
+            &PredefinedMenuItem::separator(app)?,
+            &quit,
+        ],
+    )?;
 
-    SystemTray::new().with_menu(tray_menu)
-}
+    let sm = service_manager.clone();
+    let _tray = TrayIconBuilder::new("main")
+        .icon(app.default_window_icon().unwrap().clone())
+        .menu(&menu)
+        .on_menu_event(move |app, event| {
+            match event.id.as_ref() {
+                "quit" => {
+                    sm.stop_server().ok();
+                    app.exit(0);
+                }
+                "show" => {
+                    if let Some(w) = app.get_webview_window("main") {
+                        let _ = w.show();
+                        let _ = w.set_focus();
+                    }
+                }
+                "start" => {
+                    if let Err(e) = sm.start_server() {
+                        error!("启动服务失败: {}", e);
+                    }
+                }
+                "stop" => {
+                    if let Err(e) = sm.stop_server() {
+                        error!("停止服务失败: {}", e);
+                    }
+                }
+                "open_browser" => {
+                    let port = sm.server_port();
+                    let url = format!("http://localhost:{}", port);
+                    if let Err(e) = open::that(&url) {
+                        error!("打开浏览器失败: {}", e);
+                    }
+                }
+                "toggle_widget" => {
+                    let _ = app.emit("toggle-widget", ());
+                }
+                _ => {}
+            }
+        })
+        .on_tray_icon_event(|tray, event| {
+            if let TrayIconEvent::Click {
+                button: MouseButton::Left,
+                button_state: MouseButtonState::Up,
+                ..
+            } = event
+            {
+                let app = tray.app_handle();
+                if let Some(w) = app.get_webview_window("main") {
+                    let _ = w.show();
+                    let _ = w.set_focus();
+                }
+            }
+        })
+        .build(app)?;
 
-pub fn create_tray_handler(
-    service_manager: Arc<ServiceManager>,
-) -> impl Fn(&tauri::AppHandle, SystemTrayEvent) {
-    move |app, event| match event {
-        SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-            "quit" => {
-                service_manager.stop_server().ok();
-                app.exit(0);
-            }
-            "show" => {
-                if let Some(window) = app.get_window("main") {
-                    let _ = window.show();
-                    let _ = window.set_focus();
-                }
-            }
-            "start" => {
-                if let Err(e) = service_manager.start_server() {
-                    error!("启动服务失败: {}", e);
-                }
-            }
-            "stop" => {
-                if let Err(e) = service_manager.stop_server() {
-                    error!("停止服务失败: {}", e);
-                }
-            }
-            "open_browser" => {
-                let port = service_manager.server_port();
-                let url = format!("http://localhost:{}", port);
-                if let Err(e) = open::that(&url) {
-                    error!("打开浏览器失败: {}", e);
-                }
-            }
-            _ => {}
-        },
-        SystemTrayEvent::LeftClick { .. } => {
-            if let Some(window) = app.get_window("main") {
-                let _ = window.show();
-                let _ = window.set_focus();
-            }
-        }
-        _ => {}
-    }
+    Ok(())
 }
